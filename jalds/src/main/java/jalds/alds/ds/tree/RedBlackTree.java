3557699730b14de1db010e1393b6901c5fff13d8
devender.gollapally <devender.gollapally@8a36b222-7c4b-0410-a184-67d9de0505fd>
2008-04-24 11:42:42 +0000

    refining node equals method

--- jalds/src/main/java/jalds/alds/ds/tree/RedBlackTree.java
+++ jalds/src/main/java/jalds/alds/ds/tree/RedBlackTree.java
@@ -32,10 +32,59 @@ import jalds.alds.SortableObject;
  */
 public class RedBlackTree extends BinaryTree {
 
+	private static final Node NilNode = new Node();
+	static {
+		NilNode.setNodeType(NodeType.Black);
+	}
+
+	public RedBlackTree() {
+		root = NilNode;
+	}
+
 	@Override
 	public void deleteNodeWithValue(int value) {
-		// TODO Auto-generated method stub
 
+	}
+
+	public Node insertNode(SortableObject sortableObject, boolean allowDuplicates) {
+		if (!allowDuplicates && find(root, sortableObject.getValue()) != null) {
+			return null;
+		}
+		if (root == null) {
+			root = new Node();
+			root.setSortableObject(sortableObject);
+			return root;
+		} else {
+			return checkAndCreate(root, sortableObject);
+		}
+	}
+
+	private Node checkAndCreate(Node node, SortableObject sortableObject) {
+		// should I go right or left ?
+		if (sortableObject.getValue() >= node.getSortableObject().getValue()) {
+			if (node.getRight() == null) {
+				Node right = createNode(node, sortableObject);
+				node.setRight(right);
+				return right;
+			} else {
+				return checkAndCreate(node.getRight(), sortableObject);
+			}
+		} else {
+			if (node.getLeft() == null) {
+				Node left = createNode(node, sortableObject);
+				node.setLeft(left);
+				return left;
+			} else {
+				return checkAndCreate(node.getLeft(), sortableObject);
+			}
+		}
+	}
+
+	private Node createNode(Node parent, SortableObject sortableObject) {
+		Node node = new Node();
+		node.setParent(parent);
+		node.setSortableObject(sortableObject);
+		return node;
 	}
 
 }
