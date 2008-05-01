c9a9f6d2925c2234930583f9b6488b10ae66b8a4
devender.gollapally <devender.gollapally@8a36b222-7c4b-0410-a184-67d9de0505fd>
2008-05-01 21:07:58 +0000

    fixing bugs

--- jalds/src/test/java/test/jalds/alds/ds/tree/rb/TestRedBlackTree.java
+++ jalds/src/test/java/test/jalds/alds/ds/tree/rb/TestRedBlackTree.java
@@ -20,83 +20,82 @@ public class TestRedBlackTree extends TestCase {
 			redBlackTree.insertNode(new SortableObject(key, key), allowDups);
 		}
 	}
-	
-	public void testSimple()
-	{
-		createRedBlackTree(true, 10);
+
+	protected void setUp() throws Exception {
+		createRedBlackTree(true, 20);
+	}
+
+	public void testSimple() {
 		assertNotNull(redBlackTree);
 	}
-//
-//	protected void setUp() throws Exception {
-//		createRedBlackTree(true, 20);
-//	}
-//
-//	public void testLargeTree() {
-//		createRedBlackTree(false, 1000);
-//		orderTest(redBlackTree.inOrder());
-//	}
 
-	// public void testNoDups() {
-	// createRedBlackTree(false, 20);
-	// List<SortableObject> list = redBlackTree.inOrder();
-	// for (SortableObject object : list) {
-	// int numberOfElementsWithSameValue = 0;
-	// for (int i = 0; i < list.size(); i++) {
-	// if (object.getValue() == list.get(i).getValue()) {
-	// numberOfElementsWithSameValue++;
-	// }
-	// }
-	// assertEquals(1, numberOfElementsWithSameValue);
-	// }
-	// }
-	//
-	// public void testAllOrder() {
-	// List<SortableObject> inOrder = redBlackTree.inOrder();
-	// List<SortableObject> postOrder = redBlackTree.postOrder();
-	// List<SortableObject> preOrder = redBlackTree.preOrder();
-	// assertTrue(inOrder.size() > 0);
-	// assertTrue(inOrder.size() == postOrder.size());
-	// assertTrue(postOrder.size() == preOrder.size());
-	// }
-	//
-	// public void testFind() {
-	// List<SortableObject> list = redBlackTree.inOrder();
-	// for (SortableObject object : list) {
-	// assertNotNull(redBlackTree.find(object.getValue()));
-	// }
-	// }
-	//
-	// public void testMax() {
-	// List<SortableObject> list = redBlackTree.inOrder();
-	// SortableObject maxKnown = list.get(list.size() - 1);
-	// SortableObject max = redBlackTree.findMax();
-	// assertEquals(maxKnown.getValue(), max.getValue());
-	// }
-	//
-	// public void testMin() {
-	// List<SortableObject> list = redBlackTree.inOrder();
-	// SortableObject minKnown = list.get(0);
-	// SortableObject min = redBlackTree.findMin();
-	// assertEquals(minKnown.getValue(), min.getValue());
-	// }
-	//
-	// public void testSuccessor() {
-	// List<SortableObject> list = redBlackTree.inOrder();
-	// for (int i = 0; i < list.size() - 2; i++) {
-	// int key = list.get(i).getValue();
-	// SortableObject object = redBlackTree.findSuccessor(key);
-	// assertTrue(object.getValue() >= key);
-	// }
-	// }
-	//
-	// public void testPredecessor() {
-	// List<SortableObject> list = redBlackTree.inOrder();
-	// for (int i = list.size() - 1; i > 0; i--) {
-	// int key = list.get(i).getValue();
-	// SortableObject object = redBlackTree.findPredecessor(key);
-	// assertTrue(object.getValue() <= key);
-	// }
-	// }
+	public void testLargeTree() {
+		createRedBlackTree(false, 1000);
+		orderTest(redBlackTree.inOrder());
+	}
+
+	 public void testNoDups() {
+		createRedBlackTree(false, 20);
+		List<SortableObject> list = redBlackTree.inOrder();
+		for (SortableObject object : list) {
+			int numberOfElementsWithSameValue = 0;
+			for (int i = 0; i < list.size(); i++) {
+				if (object.getValue() == list.get(i).getValue()) {
+					numberOfElementsWithSameValue++;
+				}
+			}
+			assertEquals(1, numberOfElementsWithSameValue);
+		}
+	}
+	
+	 public void testAllOrder() {
+		List<SortableObject> inOrder = redBlackTree.inOrder();
+		List<SortableObject> postOrder = redBlackTree.postOrder();
+		List<SortableObject> preOrder = redBlackTree.preOrder();
+		assertTrue(inOrder.size() > 0);
+		assertTrue(inOrder.size() == postOrder.size());
+		assertTrue(postOrder.size() == preOrder.size());
+	}
+	
+	 public void testFind() {
+		List<SortableObject> list = redBlackTree.inOrder();
+		for (SortableObject object : list) {
+			assertNotNull(redBlackTree.find(object.getValue()));
+		}
+	}
+	
+	 public void testMax() {
+		List<SortableObject> list = redBlackTree.inOrder();
+		SortableObject maxKnown = list.get(list.size() - 1);
+		SortableObject max = redBlackTree.findMax();
+		assertEquals(maxKnown.getValue(), max.getValue());
+	}
+	
+	 public void testMin() {
+		List<SortableObject> list = redBlackTree.inOrder();
+		SortableObject minKnown = list.get(0);
+		SortableObject min = redBlackTree.findMin();
+		assertEquals(minKnown.getValue(), min.getValue());
+	}
+	
+	 public void testSuccessor() {
+		List<SortableObject> list = redBlackTree.inOrder();
+		for (int i = 0; i < list.size() - 2; i++) {
+			int key = list.get(i).getValue();
+			SortableObject object = redBlackTree.findSuccessor(key);
+			assertTrue(object.getValue() >= key);
+		}
+	}
+	
+//	 public void testPredecessor() {
+//		List<SortableObject> list = redBlackTree.inOrder();
+//		for (int i = list.size() - 1; i > 0; i--) {
+//			System.out.println(i);
+//			int key = list.get(i).getValue();
+//			SortableObject object = redBlackTree.findPredecessor(key);
+//			assertTrue(object.getValue() <= key);
+//		}
+//	}
 	//
 	// public void testInOrder() {
 	// // walking in order should produce a sorted list
